eef6883f89806073bb57222703227db47412ca8f
Emerson Yurivilca <emersonyuri32@gmail.com>
2020-04-29 11:00:49 -0500

    solution with validation of numbers between to sets

--- 14.between-two-sets.py
+++ 14.between-two-sets.py
@@ -1,3 +1,5 @@
+import functools
+
 m = 2
 n = 3
 
@@ -7,18 +9,47 @@ bb = [16, 32, 96]
 aa = [2, 6]
 bb = [24, 36]
 
-a = [3, 4]
-b = [24, 48]
+ax = [3, 4]
+bx = [24, 48]
+
+
+aq = [3, 4, 6]
+bq = [12, 24, 48]
+
+a = [100, 99, 98, 97, 96, 95, 94, 93, 92, 91]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def gcd(x, y):
+    while x != y:
+        if x > y:
+            x -= y
+        else:
+            y -= x
+
+    return x
+
+
+def lcm(x, y):
+    return (int(x * y / gcd(x, y)))
 
 
 def getTotalX(a, b):
-    n = len(a)
-    m = len(b)
+    counter = 0
 
-    if n >= 1 and m >= 1 and n <= 10 and m <= 10:
-        for asLcm in range(1, n):
-            for bsGcd in range(1, m):
-                print(f"a: {a[asLcm]} & b: {b[bsGcd]}")
+    if len(a) >= 1 and len(b) >= 1 and len(a) <= 10 and len(b) <= 10:
+        aLcm = functools.reduce(lambda x, y: lcm(x, y) if (
+            x >= 1 and y >= 1 and x <= 100 and y <= 100) else False, a)
+        bGcd = functools.reduce(lambda x, y: gcd(x, y) if (
+            x >= 1 and y >= 1 and x <= 100 and y <= 100) else False, b)
+
+        for i in range(1, bGcd + 1):
+            x = aLcm * i
+            if x <= bGcd and x != 0:
+                if bGcd % x == 0:
+                    counter += 1
+
+        print(counter)
 
 
 getTotalX(a, b)
