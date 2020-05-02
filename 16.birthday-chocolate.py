ed518526faacd7945982167896f21d28bd3bfa6d
Emerson Yurivilca <emersonyuri32@gmail.com>
2020-05-02 10:17:10 -0500

    challenge solved

--- 16.birthday-chocolate.py
+++ 16.birthday-chocolate.py
@@ -1,5 +1,5 @@
 
-li = [1, 2, 1, 3, 2]
+li1 = [1, 2, 1, 3, 2]
 d1 = 3
 m1 = 2
 # r = 2
@@ -21,30 +21,11 @@ m4 = 6
 
 
 def birthday(s, d, m):
-    n = len(s)
-    print(n)
     counter = 0
-    summing = 0
-    if n >= 1 and n <= 100 and d >= 1 and d <= 31 and m >= 1 and m <= 12:
-        if m == 1 and d == s[0]:
+    for i, num in enumerate(s):
+        print(f"sum: {sum(s[i:i+m])}")
+        if sum(s[i:i+m]) == d:
             counter += 1
-        elif m == 1 and d != s[0]:
-            counter = 0
-        else:
-            for i in range(n-1):
-                if s[i] >= 1 and s[i] <= 5:
-                    # amount squares (m) summing (d)   m=2(squares)  = d(3)
-                    # show how segments meet the criteria
-                    for j in range(m):
-                        summing += s[i+j]
-
-                    print(f"************  => sum[i{i}+j{j}] = {summing} ")
-
-                    if summing == d:
-                        counter += 1
-                    print(f"Sum s[{i}] + s[{i+1}]  = {summing}")
-
-                    summing = 0
 
     print(counter)
 
